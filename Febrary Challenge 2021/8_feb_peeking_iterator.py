a0e43d4dfd09edd9edb593e54b373135ed678890
ArunRathi <arunrathi201@gmail.com>
2021-02-09 11:29:56 +0530

    8 feb problem added

--- Febrary Challenge 2021/8_feb_peeking_iterator.py
+++ Febrary Challenge 2021/8_feb_peeking_iterator.py
@@ -14,11 +14,36 @@ Calling hasNext() after that should return false.
 Follow up: How would you extend your design to be generic and work with all types, not just integer?
 
 """
-from iterator import iterator2
+
+# Below is the interface for Iterator, which is already defined for you.
 
 
-class PeekingIterator:
-    def __init__(self, iterator2):
+class Iterator:
+    def __init__(self, nums):
+        """
+        Initializes an iterator object to the beginning of a list.
+        :type nums: List[int]
+        """
+        print('hi1')
+        self.nums = nums
+
+    def hasNext(self):
+        """
+        Returns true if the iteration has more elements.
+        :rtype: bool
+        """
+        return True
+
+    def next(self):
+        """
+        Returns the next element in the iteration.
+        :rtype: int
+        """
+        return self.nums[0]
+
+
+class PeekingIterator(Iterator):
+    def __init__(self):
         """
         Initialize your data structure here.
         :type iterator: Iterator
