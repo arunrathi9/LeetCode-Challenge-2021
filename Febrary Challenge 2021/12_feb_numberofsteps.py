a3e0b5746da1f571b54919d5b57298e8bd2c2f20
ArunRathi <arunrathi201@gmail.com>
2021-02-12 16:38:12 +0530

    12 feb problem solution added

---
+++ Febrary Challenge 2021/12_feb_numberofsteps.py
@@ -0,0 +1,39 @@
+"""
+Given a non-negative integer num, return the number of steps to reduce it to zero.
+If the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+
+Example 1:
+Input: num = 14
+Output: 6
+Explanation: 
+Step 1) 14 is even; divide by 2 and obtain 7. 
+Step 2) 7 is odd; subtract 1 and obtain 6.
+Step 3) 6 is even; divide by 2 and obtain 3. 
+Step 4) 3 is odd; subtract 1 and obtain 2. 
+Step 5) 2 is even; divide by 2 and obtain 1. 
+Step 6) 1 is odd; subtract 1 and obtain 0.
+
+"""
+
+
+class Solution:
+    def numberOfSteps(self, num: int):
+
+        count = 0
+        while num > 0:
+            if num % 2 == 0:
+                num /= 2
+            else:
+                num -= 1
+            count += 1
+
+        return count
+
+    # method 2
+    def lambda_(self, num):
+        return bin(num).count('1') + len(bin(num)) - 3
+
+
+sol = Solution()
+print(sol.numberOfSteps(14))
+print(sol.lambda_(14))
