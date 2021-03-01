72d00d50266170191404a837a9363302f0653ef4
arun <arunrathi201@gmail.com>
2021-03-01 16:44:56 +0530

    added docstring in 1 march problem

--- March Challenge 2021/1_Mar_distribute_candies.py
+++ March Challenge 2021/1_Mar_distribute_candies.py
@@ -8,6 +8,14 @@ Output: 3
 
 class Solution:
     def distributeCandies(self, candyType):
+        """Return the maximum number of different types of 
+        candies she can eat if she only eats n / 2 of them.
+
+        :param candyType: array
+        :type candyType: list
+        :rtype: int
+        :return: maximum number of different types of candies one can eat
+        """
         n = len(candyType)//2
         candy_types = len(set(candyType))
         return n if n<=candy_types else candy_types
