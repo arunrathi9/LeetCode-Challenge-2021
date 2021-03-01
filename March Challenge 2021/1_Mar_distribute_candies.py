daf2786c00fe444e22f9b50d3d388880d1c36e72
arun <arunrathi201@gmail.com>
2021-03-01 16:29:37 +0530

    1 March solution added

---
+++ March Challenge 2021/1_Mar_distribute_candies.py
@@ -0,0 +1,22 @@
+"""
+Given the integer array candyType of length n, return the maximum number of different types of
+candies she can eat if she only eats n / 2 of them.
+
+Input: candyType = [1,1,2,2,3,3]
+Output: 3
+"""
+
+class Solution:
+    def distributeCandies(self, candyType):
+        n = len(candyType)//2
+        candy_types = len(set(candyType))
+        return n if n<=candy_types else candy_types
+
+
+sol = Solution()
+print(sol.distributeCandies([1,1,2,2,3,3]))
+print(sol.distributeCandies([1,1,2,3]))
+print(sol.distributeCandies([1,1,1,1]))
+print(sol.distributeCandies([1,2,3,4]))     
+
+        
