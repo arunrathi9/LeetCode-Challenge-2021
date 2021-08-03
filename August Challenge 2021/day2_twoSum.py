c3c959aea9d1a5df3633dc827a84242ab615ac0e
arunrathi9 <arunrathi201@gmail.com>
2021-08-03 12:05:00 +0530

    day 2 solution added

--- August Challenge 2021/day2_twoSum.py
+++ August Challenge 2021/day2_twoSum.py
@@ -1,6 +1,23 @@
+# Time: O(n)
+# Space: O(n)
+
+class Solution2:
+    def twoSum(self, nums, target):
+        d = dict()
+        for i in range(len(nums)):
+            temp = target - nums[i]
+            if temp in d:
+                return [d[temp], i]
+            else:
+                d[nums[i]] = i
+print('Solution 2 : ')
+sol2 = Solution2()
+print(sol2.twoSum([2,7,11,19], 9))
+print(sol2.twoSum([3,2,4], 6))
+        
 # Time: O(n^2)
 # Space: O(1)
-
+# it's a brute force method
 class Solution:
     def twoSum(self, nums, target):
         for i in range(len(nums)):
@@ -8,14 +25,6 @@ class Solution:
                 if (target-nums[i]-nums[j])==0:
                     return [i,j]
 
-sol = Solution()
-print(sol.twoSum([2,7,11,19], 9))
-print(sol.twoSum([3,2,4], 6))
-
-# Time: O(n)
-# Space: O(1)
-
-class Solution2:
-    def twoSum(self, nums, target):
-        pass
-        
+#sol = Solution()
+#print(sol.twoSum([2,7,11,19], 9))
+#print(sol.twoSum([3,2,4], 6))
